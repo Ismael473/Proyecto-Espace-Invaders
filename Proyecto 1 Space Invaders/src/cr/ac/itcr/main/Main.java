25d552da67d1d99ce2a5b6de592c5a39ac2e132d
ismael473 <ismaelmena73@gmail.com>
2021-04-08 00:05:36 -0600

    Comentarios en el Main

--- Proyecto 1 Space Invaders/src/cr/ac/itcr/main/Main.java
+++ Proyecto 1 Space Invaders/src/cr/ac/itcr/main/Main.java
@@ -6,12 +6,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame pantalla = new JFrame();
-        pantalla.setBounds(10, 10, 700,600);
-        pantalla.setTitle("DUCK INVADERS");
-        pantalla.setResizable(false);
-        pantalla.setVisible(true);
-        pantalla.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame pantalla = new JFrame(); // Se inicializa la variable de pantalla
+        pantalla.setBounds(10, 10, 700,600); //tamaño de pantalla
+        pantalla.setTitle("DUCK INVADERS"); //titulo de la pantalla
+        pantalla.setResizable(false); // Bloqueo de ajuste de tamaño de pantalla
+        pantalla.setVisible(true); // manda la ventana al frente y la hace un objeto modificable
+        pantalla.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Cuando se cierra la ventana se termina el programa
 
     }
 }
